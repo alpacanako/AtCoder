6dacf818d64b4dd3ea1dfdba121011e9039e8812
alpacanako <kb.kanako@gmail.com>
2018-06-28 20:30:53 +0900

    add ABC097-B WA

---
+++ ABC097/B.py
@@ -0,0 +1,20 @@
+def beki(n,X):
+    # the maximum of the form n**k under X
+    x = 1
+    while x*n <= X:
+        x *= n
+    if x == n:
+        return 1
+    else:
+        return(x)
+
+X = int(input())
+a = 1
+
+for n in range(X)[2:]:
+    b = beki(n,X)
+    print(n,b)
+    if b > a:
+        a = b
+
+print(a)
