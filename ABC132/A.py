0ca684506d74da346b038493832a8c105834ee70
alpacanako <kb.kanako@gmail.com>
2019-07-05 22:09:16 +0900

    create A.py

--- ABC132/A.py
+++ ABC132/A.py
@@ -1,1 +1,9 @@
-print('Hello python')
+S = list(input())
+str = "No"
+if S[0] == S[1]:
+    if S[2] == S[3]:
+        str = "Yes"
+else:
+    if S.count(S[0]) == 2 and S.count(S[1]) == 2:
+        str = "Yes"
+print(str)
