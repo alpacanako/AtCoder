dabb90f9f44d797c3a188d850a89d7f10e15eb04
alpacanako <kb.kanako@gmail.com>
2019-07-07 13:57:59 +0900

    updated C.py

--- ABC131/C.py
+++ ABC131/C.py
@@ -5,20 +5,6 @@ A,B,C,D = map(int,input().split())
 gcd = math.gcd(C,D)
 lcm = int(C*D/gcd)
 
-r = A%C
-div_c = int((B-A+1+ +C-r)//C)
-if r == 0:
-    div_c += 1
-
-r = A%D
-if r == 0:
-    r = -D
-div_d = int((B-A+1+ +D-r)//D)
-
-r = A%lcm
-if r == 0:
-    r = -lcm
-div_cd = int((B-A+1+ +lcm-r)//lcm)
-
-print(div_c, div_d, div_cd)
-print(B-A+1 -div_c -div_d +div_cd)
+ans_A = int((A-1) - (A-1)//C - (A-1)//D + (A-1)//lcm)
+ans_B = int(B - B//C - B//D + B//lcm)
+print(ans_B - ans_A)
